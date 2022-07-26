5dc8914dd97d202df13d3a634d009432fd74d96f
Rhidios <nmpreiti@gmail.com>
2022-07-26 19:09:37 -0300

    fix logout redirect, fix logout/login button

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+import time
+from django.http import HttpResponseRedirect
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
